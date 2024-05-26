5ccc87205e56f9089fdcf2deb6f61a2d0ff993a5
ishant <ishantkumar1412@gmail.com>
2024-05-26 13:36:43 +0530

    Added comments for creational_factory

--- src/DesignPatterns/creational/factory/Main.java
+++ src/DesignPatterns/creational/factory/Main.java
@@ -1,5 +1,13 @@
 package DesignPatterns.creational.factory;
 
+/**
+ * 
+ * @author ishant
+ * -> Comes under creational design pattern(where we are creating object)
+ * -> use cases:
+ * 		-> if you want to provide a particular DB connection to client based on given types from the Database
+ *         Connection Factoris, you can use factory design pattern.
+ */
 public class Main {
 	public static void main(String[] args) {
 		DBConnection dbConnection = DBConnectionFactory.getDBConnection("MONGO");
